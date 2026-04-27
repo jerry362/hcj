e0c6eab5db66c9736370239b91cc6536854b478f
lwldnjs33 <lwldnjs33@gmail.com>
2026-04-27 13:48:13 +0900

    자바스크립트

---
+++ js/test.js
@@ -0,0 +1,9 @@
+let kor = 50;
+let mat = 60;
+let eng = 70;
+let tot = kor + mat + eng;
+let avg = tot / 3; // let too = (kor + mat + eng) / 3; , 두 식이 같아요
+console.log(tot);
+// 합계
+console.log(avg);
+// 평균
